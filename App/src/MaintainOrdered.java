02b7d3d344621dc87c7c682382bd43100b041ca5
naveenvenkatsai <naveenvenkatsai28@gmail.com>
2026-04-07 15:24:57 +0530

    Maintain Ordered Bogie IDs

---
+++ App/src/MaintainOrdered.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class MaintainOrdered {
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("UC4 - Maintain Ordered Bogie Consist");
+        System.out.println("===================================\n");
+
+        LinkedList<String> trainConsist = new LinkedList<>();
+
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
+
+        trainConsist.add(2, "Pantry");
+
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
+
+        System.out.println("Final Train Consist:");
+        for (String bogie : trainConsist) {
+            System.out.println(bogie);
+        }
+    }
+}
