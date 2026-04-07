d28c58a05794c0173bdcf8948ed300afe120cfae
naveenvenkatsai <naveenvenkatsai28@gmail.com>
2026-04-07 15:20:37 +0530

    Track Unique Bogie IDs

---
+++ App/src/TrackUnique.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TrackUnique {
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println("UC3 - Track Unique Bogie IDs");
+        System.out.println("====================================\n");
+
+        Set<String> bogies = new HashSet<>();
+
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        bogies.add("BG101");
+        bogies.add("BG102");
+
+        System.out.println("Unique Bogie IDs:");
+        for (String id : bogies) {
+            System.out.println(id);
+        }
+    }
+}
